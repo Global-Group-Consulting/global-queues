00eeedef7418a2f78c1f0ebf28df937e9b5519e3
leicaflorian <florian.leica@gmail.com>
2022-02-17 17:09:58 +0100

    Updates a bunch of stuff

--- app/Jobs/TriggerBriteRecapitalization.php
+++ app/Jobs/TriggerBriteRecapitalization.php
@@ -19,11 +19,6 @@ class TriggerBriteRecapitalization extends BasicJob {
   protected $data;
   
   /**
-   * @var JobList
-   */
-  protected $jobSettings;
-  
-  /**
    * Create a new job instance.
    *
    * @param  array{userId: string, amountEuro: float, amount: integer}  $data
@@ -31,7 +26,8 @@ class TriggerBriteRecapitalization extends BasicJob {
    * @return void
    */
   public function __construct(array $data) {
-    $this->data = $data;
+    $this->data     = $data;
+    $this->selfName = self::class;
   }
   
   /**
@@ -41,42 +37,6 @@ class TriggerBriteRecapitalization extends BasicJob {
    * @throws ManuallyFailedException | Exception
    */
   public function handle() {
-    $selfName          = self::class;
-    $this->jobSettings = JobList::where("class", "=", $selfName)->first();
-    
-    if ( !$this->jobSettings) {
-      throw new ManuallyFailedException("Can't find configuration for this job");
-    }
-    
-    $payload = unserialize($this->job->payload()['data']['command']);
-    $method  = $this->jobSettings->apiMethod ?? "post";
-    $url     = $this->jobSettings->apiUrl;
-    $headers = $this->jobSettings->apiHeaders;
-    
-    if ( !$headers) {
-      $headers = [];
-    } else {
-      $headers = json_decode($headers, true);
-    }
-    
-    try {
-      /**
-       * @var Response
-       */
-      $result = Http::withOptions([])
-        ->withHeaders($headers)
-        ->$method($url, $payload->data);
-  
-      if ($result->failed()) {
-        throw new Exception(json_encode($result->json()));
-      }
-  
-      $this->job->respData = $result->body();
-      $this->job->reqData  = json_encode($payload->data);
-    } catch (Exception $exception) {
-      dump($exception);
-      throw new Exception($exception->getMessage());
-    }
-    
+    $this->makeHttpCall(self::class);
   }
 }
