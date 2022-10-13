feee81ec31e8841983288a35376b2eb1b589b96c
leicaflorian <florian.leica@gmail.com>
2022-10-13 19:35:02 +0200

    Adds new Jobs

---
+++ app/Jobs/TriggerCheckPackExpiration.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Classes\BasicJob;
+
+class TriggerCheckPackExpiration extends BasicJob {
+  public $data;
+
+  /**
+   * Create a new job instance.
+   *
+   * @return void
+   */
+  public function __construct($data) {
+    $this->data = $data;
+    $this->selfName = self::class;
+  }
+  
+  /**
+   * Execute the job.
+   *
+   * @return void
+   */
+  public function handle() {
+    $this->makeHttpCall(self::class);
+  }
+}
